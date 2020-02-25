a2de143946f2e8a34130f1f68b49643f098b7d4c
mohammed bouanane <mbouanane3@gmail.com>
2020-02-25 18:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  let arr =[];
-  for (let i=0; i < musicians.length; i++) {
-    const str = musicians[i] + 'plays' + instruments[i];
-    arr.push(str);
-  }
-  return arr;
-}
